dad1b0bec1a7c3dfde9ff746d149719ea4ca6002
JamesNewton <jamesnewton@massmind.org>
2025-12-07 22:34:51 -0800

    Refactor register handling, CALL has correct value
    Refactor register handling, figure out correct value to call to.

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 // https://massmind.org/techref/idea/minimalcontroller.htm
 
 #include <stdio.h>
+#include <cstdint>
 //yeah I know. But I think these are cool, so:
 #define TRUE  (1==1)
 #define FALSE (!TRUE)
@@ -10,26 +11,38 @@
 //A size of 4 means 32 bit operations will be possible. 8 would be 64.
 //This doesn't matter until the op width can be set.
 #define REG_SIZE 4
+#if REG_SIZE == 4
+#define SIZE_TYPE int32_t
+#else
+#error
+#endif
+
 //size of the program memory
 #define MEM_SIZE 1000
-//amount of memory to include after 'z' register. Stack grows here
+//amount of memory to include after 'z' register.
 #define STACK_SIZE 100
 int num;
 char dst, src; 
 unsigned char op;
 char flag;
-#define REG_SIZE_CHARS (1+'z'-'a')*REG_SIZE+STACK_SIZE
+#define REG_SIZE_CHARS (1+'z'-'a')*REG_SIZE
 #define REG_SIZE_INTS REG_SIZE_CHARS / sizeof(int)
 union reg_t { 
     char c[REG_SIZE_CHARS]; 
     int i[REG_SIZE_INTS]; 
     } reg;
 //1 indexed array of registers. 0 is "no register found"
+//register letter to address in reg array. 1 based so 0 is false.
+#define REG(x) ((1+x-'a')*REG_SIZE)
+#define REGI(x) ((1+x-'a'))
 //Code memory for programs. Could be FLASH or EEPROM
 char mem[MEM_SIZE];
+//Stack
+int stack[STACK_SIZE];
 char *radix; //radix. "r" in the register array
 int *pc; //program counter pointer. "p" in the register array
-int next; //pointer to free location in memory for the next instruction. 
+int *sp;
+int next; //index of next free location in memory for the next instruction. 
 char c; //hold the current character being processed
 
 int is_white_space(char c) {
@@ -79,11 +92,11 @@ void number() {
 	}
 }
 int is_register(char c) {
-	if (c>='a' && c<='z') return TRUE;
+	if (c>='a' && c<='z') {return TRUE;}
 	return FALSE;
 }
 int is_src_dst_reg(char sd) {
-	if (sd >= 1 && sd <=26) return TRUE;
+	if (sd >= 0 && sd <=26) return TRUE;
 	return FALSE;
 }
 int registers() {
@@ -94,8 +107,8 @@ int registers() {
 		r += num; //if a number was given, offset by that amount. 4a is a+4
 		//TODO: 'a' will end up as zero, which flags no destination?
 		num = 0;
+		printf(" r:'%c' @%d.",c,r);
 		get_inst(); //if you used it, replace it.
-		printf(" r:%d.",r);
 	}
 	return r;
 
@@ -129,20 +142,33 @@ void operation() {
 		if ('=' == c && '>' == op) { op = 'G'; get_inst();} //greater than or equal
 		if ('\"' == op) { printf("\nString:\"");//quote
 			if (is_src_dst_reg(dst)) { //move source to destination until EOF or ending "
-				reg.i[(int)dst] = next; //point register to memory
+				//reg.i[(int)dst] = next; //point register to memory
+                num = next;
 				while(('\"' != c) && (EOF != c)) { printf("%c", c); //TODO also check *pc
+                    if (next > sizeof(mem)) { printf("\nOOM!");
+                        return;
+                    }
 					mem[next++] = c;
+                    ;
 					get_inst();
 				} 
-			get_inst(); //move past closing quote
+			if (c == '\"') get_inst(); //move past closing quote
 			printf("\"");
 			}
 		}
+        if (')' == op) {
+            stack[*sp] = *pc;
+            *sp++;
+            //*pc = src_val;
+            num = reg.i[(int)dst/REG_SIZE];;
+            dst = REG('p');
+            printf("\nCALL %d = %d", dst, num);
+        }
 		printf(" op:%d.c:%d",op,c);
 	 }
 }
 void source() {
-	src = registers();
+	if (!src) src = registers();
 	if (!src) src = devices();
 	printf(" src:%d.", src);
 }
@@ -150,17 +176,18 @@ int read_device(int src) {
 	//TODO read devices. 
 	return 0;
 }
+
 void do_it() {
 	//convert src and dst into the values at thier addresses for width
 	//TODO support widths
 	int src_val = num;
 	if (src <= sizeof(reg.c)) { //register
 		src_val = reg.i[(int)src] + num; 
-        printf("src:%d %c val:%d. ", src, src/REG_SIZE, src_val);
 	} else { //otherwise it's a device
 		src_val = read_device(src);
 	}
-	int dst_val = reg.i[(int)dst]; 
+	int dst_val = reg.i[(int)dst/REG_SIZE]; 
+    printf("do_it src:%d %c val:%d. dstv:%d ", src, src/REG_SIZE+'a'-1, src_val, dst_val);
 	//use num if no source
 	switch(op) {
 		case ':': dst_val = src_val; break;
@@ -177,19 +204,21 @@ void do_it() {
 		case '<': flag = (dst_val < src_val); break;
 		case 'G': flag = (dst_val >= src_val); break;
 		case 'L': flag = (dst_val <= src_val); break;
+        case '\"': dst_val = num; break;
 		case '(': break; //parameter list (register indirection)
-		case ')': break; //call
+		case ')': 
+            break; //call
 	}
 	//TODO support widths
 	if (dst <= sizeof(reg.c)) { //register
-		reg.i[(int)dst] = dst_val;
+		reg.i[(int)dst/REG_SIZE] = dst_val;
 	} else { //otherwise it's a device
 		switch(dst-FIRST_DEVICE+'A') {
 			case 'P': //Port/pin
 				break;
 		}
 	}
-	printf("dst:%d %c val:%d.", dst, dst/REG_SIZE, dst_val);
+	printf("dst:%d %c val:%d.", dst, dst/REG_SIZE+'a'-1, dst_val);
 	//look ahead for conditionals //TODO should this be under operation?
 	switch(c) {
 		case '?': 
@@ -201,13 +230,21 @@ void do_it() {
             get_inst();
 			skip_line();
 			break;
-		case '.': c=0; return;//TODO up stack to top. For now, just return
+		case '.': 
+            c=0; 
+            if (*sp > 0) {
+                *sp--; //s--
+                *pc = stack[*sp]; //p:[s]
+            } else {
+                *pc = 0;
+            }
+            return;//TODO up stack to top. For now, just return
 	}
 }
 
 int done() { 
 	if (dst && op &&  (src || num)) {
-		printf("\nDO: %c+%d %c= %c %d +#: %d. c:%d '%c' \n", 
+		printf(" DO: %c+%d %c= %c %d +#: %d. c:%d '%c' \n", 
             dst/REG_SIZE+'`', 
             dst % REG_SIZE, 
             op, 
@@ -220,7 +257,7 @@ int done() {
 		do_it();
 		op = src = 0;
 	}
-	if ('\n' == c) { printf(" EOL ");
+	if ('\n' == c) { printf(" EOL \n");
 		get_inst(); //if you used it, replace it.
 		num = dst = src = op = 0;
 		return TRUE; 
@@ -235,18 +272,20 @@ int done() {
 	
 }
 
-//register letter to address in reg array. 1 based so 0 is false.
-#define REG(x) ((1+x-'a')*REG_SIZE)
 
 int main(void) {
-	radix = &reg.c[REG('r')]; //register 'r' stores the radix
+	radix = (char *)&reg.c[REG('r')]; //register 'r' stores the radix
 	*radix = 10;
-	printf("radix %d (%d)\n", *radix, reg.c[REG('r')]);
-	pc = (int *)&reg.c[REG('p')]; //questionable. TODO Better way?
+	printf("radix %d (%d)\n", *radix, reg.i[REGI('r')]);
+	pc = (int *)&reg.i[REGI('p')]; //questionable. TODO Better way?
 	*pc = 0; //start accepting bytcodes from console, not memory
-	printf("program counter %d (%d)\n", *pc, reg.c[REG('p')]);
+	printf("program counter %d (%d)\n", *pc, reg.i[REG('p')]);
+	sp = (int *)&reg.i[REGI('s')]; //questionable. TODO Better way?
+	*sp = 1; //stack pointer to first element
+    stack[0] = 0; //make sure there is a zero to return to
+	printf("stack pointer %d (%d)\n", *pc, reg.i[REGI('s')]);
 	//next = (int *)&reg.c[REG('n')]; //TODO Better way?
-	next = 0;
+	next = 1; mem[0] = ' '; //start next at 1 to allow test for zero
 	dst = src = op = 0;
 	flag = FALSE;
 	get_inst(); //prime the pump
@@ -260,12 +299,13 @@ int main(void) {
 			source(); //source value
 		} while(!done()); //do we have everything? do it! repeat op and source
 	}
-	printf("mem: %s.",mem);
+	printf("\nmem: %s.",mem);
     printf("\nreg:");
+    //*pc=1;
     for(char i = 'a'; i<'z'; i++) {
-        int v = reg.i[REG(i)];
+        int v = (int )reg.i[REGI(i)];
         if (v) {
-            printf("\n%c:%d", i, v);
+            printf("\n %c:%d", i, v);
         }
     }
 	return 0;
